2cba95a3e0b27ad1ff7d9909fa1504112fb9a3c7
Kasidatep <bank@kasidate.me>
2023-01-26 20:45:00 +0700

    Project created

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
       <div class="text-3xl font-semibold m-12">PROJECT1-SEC-1-boi_046</div>
       <div class="w-full flex-col h-full justify-center">
         <img src="./assets/images/uc-icon.png" class="h-96 justify-center m-auto">
-        <div class="m-auto w-96 bg-yellow-400 p-10 rounded-xl text-3xl font-extrabold text-center">Under Constructor</div>
+        <div class="m-auto w-96 bg-yellow-400 p-10 rounded-xl text-3xl font-extrabold text-center">Under Construction</div>
       </div>
    
   </div>
